e6f28ed7093a7b6fd634c1eaced8b3ddd45b6302
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-08-19 08:33:50 +0100

    starting to add visual layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,16 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="min-h-screen bg-[#ffec8a]">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
+<style>
 
+/* 
+TODO form #372e4b
+TODO item #ea4d87
+TEXT #fffbfa
+*/
 </style>
--- frontend/src/views/TodoView.vue
+++ frontend/src/views/TodoView.vue
@@ -1,70 +1,70 @@
 <template>
-    <div>
-        {{ todoList }}
+    <div class="flex justify-center min-h-screen p-5">
+        <div class="flex justify-center w-5/6 sm:w-3/4 md:max-w-lg min-h-[100px] border border-red-500">
+            <h1>Todo List</h1>
+            <!-- TODO drop down button -->
+            <!-- TODO todo list item -->
+        </div>
+
     </div>
-    <div>{{ itemToAdd }}</div>
-    <input type="text" v-model="itemToAdd">
-    <button @click="createTodo">Add item</button>
 </template>
 
 <script>
 import { mapStores } from 'pinia'
 import { useTodoStore } from '../stores/todoStore';
 
-    export default {
-        data() {
-            return {
-                category: "",
-                itemToAdd: ""
+export default {
+    data() {
+        return {
+            category: "",
+            itemToAdd: ""
+        }
+    },
+    computed: {
+        // note we are not passing an array, just one store after the other
+        // each store will be accessible as its id + 'Store'
+        ...mapStores(useTodoStore),
+        todoList() {
+            return this.todoStore.todoList
+        }
+    },
+    methods: {
+        async addCategory() {
+            const data = {
+                "category": this.category
             }
-        },
-        computed: {
-            // note we are not passing an array, just one store after the other
-            // each store will be accessible as its id + 'Store'
-            ...mapStores(useTodoStore),
-            todoList() {
-                return this.todoStore.todoList
-            }
-        },
-        methods: {
-            async addCategory() {
-                const data = {
-                    "category": this.category
-                }
-                try {
-                    await this.todoStore.createTodoCategory(data)
-                } catch (error) {
-                    alert(error.message)
-                }
-            },
-            async createTodo() {
-                const data = {
-                    "category": this.category,
-                    "item": this.itemToAdd
-                }
-                try {
-                    await this.todoStore.createTodo(data)
-                    this.itemToAdd = ""
-                } catch (error) {
-                    alert(error)
-                }
-            },
-            async updateTodo() {},
-            async deleteTodo() {},
-        },
-        async mounted () {
             try {
-                await this.todoStore.fetchTodoListItems()
-                if (Object.keys(this.todoList).length > 0) {
-                    this.category = Object.keys(this.todoList)[0]
-                }
+                await this.todoStore.createTodoCategory(data)
             } catch (error) {
-                alert(`${error}`)
+                alert(error.message)
             }
         },
-    }
+        async createTodo() {
+            const data = {
+                "category": this.category,
+                "item": this.itemToAdd
+            }
+            try {
+                await this.todoStore.createTodo(data)
+                this.itemToAdd = ""
+            } catch (error) {
+                alert(error)
+            }
+        },
+        async updateTodo() { },
+        async deleteTodo() { },
+    },
+    async mounted() {
+        try {
+            await this.todoStore.fetchTodoListItems()
+            if (Object.keys(this.todoList).length > 0) {
+                this.category = Object.keys(this.todoList)[0]
+            }
+        } catch (error) {
+            alert(`${error}`)
+        }
+    },
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
